47fd068ae24076c169e7eeb8c3a7ce4ab8f35030
whyknotnow <firat.caglar.akbulut@gmail.com>
2019-11-17 14:11:55 +0300

    Add project files.

---
+++ webapi/Controllers/SendPushController.cs
@@ -0,0 +1,112 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Mvc;
+using webservapi.Models;
+
+namespace webapi.Controllers
+{
+    public class SendPushController : ApiController
+    {
+        
+
+        // POST: api/SendPush
+        public IHttpActionResult Post(PushRequestModel req)
+        {
+
+            try
+            {
+
+                var headers = Request.Headers;
+                if (!headers.Contains("api_key"))
+                {
+                    return new System.Web.Http.Results.ResponseMessageResult(
+                Request.CreateErrorResponse(
+                    (HttpStatusCode)403,
+                    new HttpError("API key bulunamadı")
+                )
+            );
+
+                }
+
+                string token = headers.GetValues("api_key").First();
+
+                if(token != "test")
+                {
+                    return new System.Web.Http.Results.ResponseMessageResult(
+               Request.CreateErrorResponse(
+                   (HttpStatusCode)401,
+                   new HttpError("API key geçersiz")
+               )
+           );
+
+                }
+
+                if (String.IsNullOrEmpty(req.platform))
+                {
+                    return new System.Web.Http.Results.ResponseMessageResult(
+                                   Request.CreateErrorResponse(
+                                       (HttpStatusCode)400,
+                                       new HttpError("Lütfen platform belirtin")
+                                   )
+                               );
+                }
+
+                if (String.IsNullOrEmpty(req.deviceToken))
+                {
+                    return new System.Web.Http.Results.ResponseMessageResult(
+                                   Request.CreateErrorResponse(
+                                       (HttpStatusCode)400,
+                                       new HttpError("Lütfen deviceToken belirtin")
+                                   )
+                               );
+                }
+
+                if (String.IsNullOrEmpty(req.title))
+                {
+                    return new System.Web.Http.Results.ResponseMessageResult(
+                                   Request.CreateErrorResponse(
+                                       (HttpStatusCode)400,
+                                       new HttpError("Lütfen title belirtin")
+                                   )
+                               );
+                }
+
+                if (String.IsNullOrEmpty(req.body))
+                {
+                    return new System.Web.Http.Results.ResponseMessageResult(
+                                   Request.CreateErrorResponse(
+                                       (HttpStatusCode)400,
+                                       new HttpError("Lütfen body belirtin")
+                                   )
+                               );
+                }
+
+
+            }
+            catch(Exception ex)
+            {
+                return new System.Web.Http.Results.ResponseMessageResult(
+               Request.CreateErrorResponse(
+                   (HttpStatusCode)500,
+                   new HttpError("Sunucu hatası, lütfen daha sonra deneyiniz.")
+               )
+           );
+            }
+
+      return new System.Web.Http.Results.ResponseMessageResult(
+                Request.CreateResponse(
+                    (HttpStatusCode) 200,
+                    "SUCCESS"
+                )
+            );
+            ;
+        }
+        
+
+    }
+}
---
+++ webapi/Models/PushRequestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace webservapi.Models
+{
+    public class PushRequestModel
+    {
+            public string platform { get; set; }
+            public string deviceToken { get; set; }
+            public string title { get; set; }
+            public string subtitle { get; set; }
+            public string body { get; set; }
+            public int badge { get; set; }
+            public string sound { get; set; }
+        
+    }
+}
